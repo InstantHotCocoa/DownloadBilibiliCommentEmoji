64b934627041ac72a7bd14b8b8a83f8aabce695b
InstantHotCocoa <zzz8176@outlook.com>
2021-08-01 05:16:07 +0800

    Add files via upload

--- DownloadBilibiliCommentEmoji.js
+++ DownloadBilibiliCommentEmoji.js
@@ -1,25 +1,28 @@
 var count = 0;
-var id = setInterval(function () {
-    var currentValue = document.querySelectorAll("a.emoji-pic")[count];
-    var url = currentValue.childNodes[0].getAttribute("src").split("@")[0];
-    var name = currentValue.childNodes[0].getAttribute("title").slice(1, -1) + ".png";
+var emojiList = document.querySelectorAll("a.emoji-pic");
+if (emojiList.length > 0) {
+    var intervalID = setInterval(function () {
+        var currentValue = emojiList[count];
+        var url = currentValue.childNodes[0].getAttribute("src").split("@")[0];
+        var name = currentValue.childNodes[0].getAttribute("title").slice(1, -1) + "." + url.split(".").pop();
+        
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'blob';
+        xhr.onload = () => {
+            if (xhr.status == 200) {
+                var urlObject = window.URL || window.webkitURL || window;
+                var elem = document.createElement("a");
+                elem.href = urlObject.createObjectURL(new Blob([xhr.response]));
+                elem.download = name;
+                elem.click();
+            }
+        };
+        xhr.send();
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'blob';
-    xhr.onload = () => {
-        if (xhr.status === 200) {
-            var urlObject = window.URL || window.webkitURL || window;
-            var elem = document.createElement("a");
-            elem.href = urlObject.createObjectURL(new Blob([xhr.response]));
-            elem.download = name;
-            elem.click();
+        count++;
+        if (count >= emojiList.length) {
+            clearInterval(intervalID);
         }
-    };
-
-    xhr.send();
-    count++;
-    if (count == document.querySelectorAll("a.emoji-pic").length) {
-        clearInterval(id)
-    }
-}, 1000);
+    }, 200);
+}
